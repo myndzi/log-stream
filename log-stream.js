2fe97f8ec3a51c0607196a79ccd800024006819e
Kris Reeves <krisreeves@searchfanatics.com>
2014-06-05 15:50:20 +0000

    Initial commit

---
+++ log-stream.js
@@ -0,0 +1,85 @@
+'use strict';
+
+var Promise = require('bluebird'),
+    stream = require('stream'),
+    PATH = require('path'),
+    util = require('util'),
+    fs = require('fs');
+
+module.exports = LogStream;
+
+function LogStream(cfg) {
+    cfg = cfg || { };
+    
+    stream.Writable.call(this);
+    
+    this.rename = cfg.rename || Promise.promisify(fs.rename);
+    this.readDir = cfg.readDir || Promise.promisify(fs.readdir);
+    this.createWriteStream = cfg.createWriteStream || fs.createWriteStream;
+    
+    this.fileName = cfg.name || 'log';
+    this.dirName = cfg.dir || 'logs';
+    this.nextNumber = 0;
+    this.maxSize = cfg.maxSize || 0x100;//000; // 1,048,576
+    
+    this.bytes = 0;
+    this.stream = null;
+}
+util.inherits(LogStream, stream.Writable);
+
+LogStream.prototype._write = function (chunk, encoding, callback) {
+    var self = this;
+    
+    return Promise.try(function () {
+        if (self.stream === null) {
+            return self.init();
+        }
+    }).then(function () {
+        self.bytes += chunk.length;
+        self.stream.write(chunk);
+        
+        if (self.bytes >= self.maxSize) {
+            return self.rotateStream();
+        }
+    }).nodeify(callback);
+};
+LogStream.prototype.init = Promise.method(function () {
+    var self = this, regex = new RegExp('^'+self.fileName);
+    
+    // find largest already-existing file
+    return self.readDir(self.dirName).filter(function (item) {
+        return regex.test(item);
+    }).reduce(function (max, item) {
+        var num = +item.split('.').pop();
+        
+        if (typeof num === 'number' && num > max) {
+            return num;
+        } else {
+            return max;
+        }
+    }, 0).then(function (max) {
+        if (typeof max !== 'number') { max = 0; }
+        self.nextNumber = max + 1;
+        return self.openStream();
+    });
+});
+LogStream.prototype.openStream = Promise.method(function () {
+    var self = this;
+    if (this.stream) {
+        console.log('WARN: LogStream.openStream called but already had a stream');
+        this.stream.end();
+    }
+    var logfile = PATH.join(this.dirName, this.fileName);
+    this.stream = this.createWriteStream(logfile, { flags: 'a' });
+});
+LogStream.prototype.rotateStream = Promise.method(function () {
+    var logfile = PATH.join(this.dirName, this.fileName),
+        archive = PATH.join(this.dirName, this.fileName + '.' + this.nextNumber);
+    
+    this.nextNumber++;
+    
+    this.stream.end();
+    this.stream = null;
+    
+    return this.rename(logfile, archive).then(this.openStream.bind(this));
+});
---
+++ test/log-stream.test.js
@@ -0,0 +1,96 @@
+var assert = require('assert'),
+    should = require('should'),
+    Stream = require('stream'),
+    Promise = require('bluebird');
+
+var LogStream = require('../log-stream');
+
+describe('LogStream', function () {
+    function dumy(obj) {
+        obj = obj || { };
+        return {
+            maxSize: obj.maxSize || 10,
+            readDir: Promise.method(function () {
+                if (obj.files) { return obj.files; }
+                return obj.readDir ? obj.readDir.apply(null, arguments) : [ ];
+            }),
+            createWriteStream: function () {
+                if (obj.stream) { return obj.stream; }
+                if (obj.streams) { return obj.streams.shift(); }
+                return (
+                    obj.createWriteStream ?
+                    obj.createWriteStream.apply(null, arguments) :
+                    new Stream.PassThrough()
+                );
+            },
+            rename: Promise.method(function () {
+                return obj.rename ? obj.rename.apply(null, arguments) : Promise.resolve();
+            })
+        };
+    }
+    
+    it('should instantiate and initialize', function () {
+        var stream = new LogStream(dumy());
+        return stream.init();
+    });
+    it('should find the latest logfile on init and store the value of the next number', function () {
+        var stream = new LogStream(dumy({
+            files: [ 'log', 'log.1', 'log.2' ]
+        }));
+        return stream.init().then(function () {
+            stream.nextNumber.should.equal(3);
+        });
+    });
+    it('should ignore gaps in the sequence', function () {
+        var stream = new LogStream(dumy({
+            files: [ 'log', 'log.5' ]
+        }));
+        return stream.init().then(function () {
+            stream.nextNumber.should.equal(6);
+        });
+    });
+    it('should initialize automatically when written to', function (done) {
+        var strim = new Stream.PassThrough();
+        var stream = new LogStream(dumy({
+            stream: strim
+        }));
+        strim.on('data', function (chunk) {
+            chunk.toString().should.equal('foo');
+            done();
+        });
+        
+        stream.write('foo');
+    });
+    it('should output to a new stream when exceeding maxSize', function (done) {
+        var strim = new Stream.PassThrough(),
+            strim2 = new Stream.PassThrough();
+        
+        var stream = new LogStream(dumy({
+            streams: [ strim, strim2 ]
+        }));
+        
+        strim.on('data', function (chunk) {
+            chunk.toString().should.equal('0123456789');
+        });
+        strim2.on('data', function (chunk) {
+            chunk.toString().should.equal('foo');
+            done();
+        });
+        
+        stream.write('0123456789');
+        stream.write('foo');
+    });
+    it('should end old stream when diverting to a new one', function (done) {
+        var strim = new Stream.PassThrough(),
+            strim2 = new Stream.PassThrough();
+        
+        var stream = new LogStream(dumy({
+            streams: [ strim, strim2 ]
+        }));
+        
+        strim.on('finish', function () { done(); });
+        
+        stream.write('0123456789');
+        stream.write('foo');
+    });
+});
